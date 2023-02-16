15346f4ceebb40cac68704ae2ecd9d204354fadc
Kiporion <meho.mustafic@outlook.com>
2023-02-16 12:24:58 +0100

    Finished design, working on PW authentication

---
+++ main.js
@@ -0,0 +1,24 @@
+const mainPW=document.getElementById("main-pw");
+const confPW=document.getElementById("conf-pw");
+function checkPassword(form){
+    pw1=form.password.value;
+    pw2=form.conf_password.value;
+
+    if (pw1 == '')
+                    alert ("Please enter Password");
+                // If confirm password not entered
+                else if (pw2 == '')
+                    alert ("Please enter confirm password");
+                      
+                // If Not same return False.    
+                else if (pw1 != pw2) {
+                    alert ("\nPassword did not match: Please try again...")
+                    return false;
+                }
+  
+                // If same return True.
+                else{
+                    alert("Password Match!")
+                    return true;
+                }
+}
